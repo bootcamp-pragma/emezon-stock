12be8fe3059eb49d0c080386f7105466ff2dcf53
Jeison David Moncayo Marzola <desarrollo5ude@udea.edu.co>
2024-08-18 22:00:07 -0500

    Initial commit

---
+++ src/test/java/com/emezon/stock/StockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emezon.stock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
