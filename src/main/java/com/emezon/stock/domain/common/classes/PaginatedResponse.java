ebf640262521320cf461f45cbacfebf68f241fcf
Jeison David Moncayo Marzola <desarrollo5ude@udea.edu.co>
2024-08-24 03:50:30 -0500

    Created custom object PaginatedResponse

---
+++ src/main/java/com/emezon/stock/domain/common/classes/PaginatedResponse.java
@@ -0,0 +1,54 @@
+package com.emezon.stock.domain.common.classes;
+
+import java.util.List;
+
+public class PaginatedResponse <T> {
+
+    private List<T> data;
+    private int page;
+    private int size;
+    private long totalItems;
+    private int totalPages;
+
+    public PaginatedResponse() { }
+
+    public List<T> getData() {
+        return data;
+    }
+
+    public void setData(List<T> data) {
+        this.data = data;
+    }
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public long getTotalItems() {
+        return totalItems;
+    }
+
+    public void setTotalItems(long totalItems) {
+        this.totalItems = totalItems;
+    }
+
+    public int getTotalPages() {
+        return totalPages;
+    }
+
+    public void setTotalPages(int totalPages) {
+        this.totalPages = totalPages;
+    }
+}
