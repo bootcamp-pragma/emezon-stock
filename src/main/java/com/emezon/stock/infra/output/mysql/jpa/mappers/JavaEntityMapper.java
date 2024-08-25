77e23461fbc848f9dc89f50345d58ca44e558ed9
Jeison David Moncayo Marzola <desarrollo5ude@udea.edu.co>
2024-08-25 16:12:13 -0500

    Created JavaEntityMapper

---
+++ src/main/java/com/emezon/stock/infra/output/mysql/jpa/mappers/JavaEntityMapper.java
@@ -0,0 +1,33 @@
+package com.emezon.stock.infra.output.mysql.jpa.mappers;
+
+import com.emezon.stock.domain.models.Category;
+import com.emezon.stock.infra.output.mysql.jpa.entities.CategoryEntity;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class JavaEntityMapper {
+
+    private JavaEntityMapper() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    public static CategoryEntity toEntity(Category category) {
+        CategoryEntity categoryEntity = new CategoryEntity();
+        categoryEntity.setId(category.getId());
+        categoryEntity.setName(category.getName());
+        categoryEntity.setDescription(category.getDescription());
+        categoryEntity.setCode(category.getCode());
+        return categoryEntity;
+    }
+
+    public static Category toModel(CategoryEntity categoryEntity) {
+        Category category = new Category();
+        category.setId(categoryEntity.getId());
+        category.setName(categoryEntity.getName());
+        category.setDescription(categoryEntity.getDescription());
+        category.setCode(categoryEntity.getCode());
+        return category;
+    }
+
+}
