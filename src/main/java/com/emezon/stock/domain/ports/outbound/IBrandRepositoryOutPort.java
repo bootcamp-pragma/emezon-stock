211f877baa19648f6a7a275dbc0ebd9bf058c5c0
Jeison David Moncayo Marzola <desarrollo5ude@udea.edu.co>
2024-08-25 15:35:28 -0500

    IBrandRepositoryOutPort

---
+++ src/main/java/com/emezon/stock/domain/ports/outbound/IBrandRepositoryOutPort.java
@@ -0,0 +1,18 @@
+package com.emezon.stock.domain.ports.outbound;
+
+import com.emezon.stock.domain.common.classes.PaginatedResponse;
+import com.emezon.stock.domain.models.Brand;
+
+import java.util.Optional;
+
+public interface IBrandRepositoryOutPort {
+
+    Brand save(Brand brand);
+
+    Optional<Brand> findById(String id);
+
+    Optional<Brand> findByName(String name);
+
+    PaginatedResponse<Brand> findAll(int page, int size, String sortDirection);
+
+}
