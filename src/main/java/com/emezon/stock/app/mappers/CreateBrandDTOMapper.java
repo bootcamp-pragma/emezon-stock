139066c1faf49bbb8817d97e7a2177136c1c53a4
Jeison David Moncayo Marzola <desarrollo5ude@udea.edu.co>
2024-08-25 20:12:06 -0500

    Created dtos mapper for BrandDTO

---
+++ src/main/java/com/emezon/stock/app/mappers/CreateBrandDTOMapper.java
@@ -0,0 +1,24 @@
+package com.emezon.stock.app.mappers;
+
+import com.emezon.stock.app.dtos.BrandDTO;
+import com.emezon.stock.app.dtos.CreateBrandDTO;
+import com.emezon.stock.domain.models.Brand;
+
+public class CreateBrandDTOMapper {
+
+    public static Brand toModel(CreateBrandDTO brand) {
+        Brand brandModel = new Brand();
+        brandModel.setName(brand.getName());
+        brandModel.setDescription(brand.getDescription());
+        return brandModel;
+    }
+
+    public static BrandDTO toDTO(Brand brand) {
+        BrandDTO brandDTO = new BrandDTO();
+        brandDTO.setId(brand.getId());
+        brandDTO.setName(brand.getName());
+        brandDTO.setDescription(brand.getDescription());
+        return brandDTO;
+    }
+
+}
