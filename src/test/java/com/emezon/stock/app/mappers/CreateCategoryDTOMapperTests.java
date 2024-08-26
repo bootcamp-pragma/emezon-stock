e3aeae68f9d4c4db56a8d74e14defb03586f885a
Jeison David Moncayo Marzola <desarrollo5ude@udea.edu.co>
2024-08-26 00:34:42 -0500

    Ceated tests for privates constructors of CreateCategoryDTOMapper and CreateBrandDTOMapper

---
+++ src/test/java/com/emezon/stock/app/mappers/CreateCategoryDTOMapperTests.java
@@ -0,0 +1,25 @@
+package com.emezon.stock.app.mappers;
+
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class CreateCategoryDTOMapperTests {
+
+    @Test
+    void CreateCategoryDTOMapper_shouldHavePrivateConstructor() throws NoSuchMethodException {
+        Constructor<CreateCategoryDTOMapper> constructor = CreateCategoryDTOMapper.class.getDeclaredConstructor();
+        assertThrows(IllegalAccessException.class, constructor::newInstance);
+    }
+
+    @Test
+    void CreateCategoryDTOMapper_shouldThrowExceptionWhenInstantiated() throws NoSuchMethodException {
+        Constructor<CreateCategoryDTOMapper> constructor = CreateCategoryDTOMapper.class.getDeclaredConstructor();
+        constructor.setAccessible(true);
+        assertThrows(InvocationTargetException.class, constructor::newInstance);
+    }
+
+}
