38347d29b794910785403e829cfe3a49bca5cee2
Jeison David Moncayo Marzola <desarrollo5ude@udea.edu.co>
2024-08-25 20:24:22 -0500

    Updated Category and Brand Error Messages

--- src/main/java/com/emezon/stock/domain/common/constants/BrandErrorMessages.java
+++ src/main/java/com/emezon/stock/domain/common/constants/BrandErrorMessages.java
@@ -2,7 +2,7 @@ package com.emezon.stock.domain.common.constants;
 
 public class BrandErrorMessages {
 
-    public static final String BRAND_NAME_ALREADY_EXISTS = "Brand with name %s already exists";
+    public static final String BRAND_NAME_ALREADY_EXISTS = "Brand with name '%s' already exists";
     public static final String BRAND_NAME_ALREADY_EXISTS_DETAILS = "Brand name must be unique. Please try again with a different name.";
     public static final String BRAND_NAME_REQUIRED = "Brand name is required";
     public static final String BRAND_NAME_MAX_LENGTH = "Brand name must not exceed %d characters";
