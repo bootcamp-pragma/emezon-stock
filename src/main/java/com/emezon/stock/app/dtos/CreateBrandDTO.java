4aca7e3286776c3fa78669690f3210988628e8db
Jeison David Moncayo Marzola <desarrollo5ude@udea.edu.co>
2024-08-25 20:09:08 -0500

    Created dtos for Brand

---
+++ src/main/java/com/emezon/stock/app/dtos/CreateBrandDTO.java
@@ -0,0 +1,34 @@
+package com.emezon.stock.app.dtos;
+
+import com.emezon.stock.domain.common.constants.BrandErrorMessages;
+import jakarta.annotation.PostConstruct;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class CreateBrandDTO {
+
+    @NotBlank(message = BrandErrorMessages.BRAND_NAME_REQUIRED)
+    private String name;
+
+    @NotBlank(message = BrandErrorMessages.BRAND_DESCRIPTION_REQUIRED)
+    private String description;
+
+    public CreateBrandDTO(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    @PostConstruct
+    public void init() {
+        if (this.name != null) {
+            this.name = this.name.trim();
+        }
+        if (this.description != null) {
+            this.description = this.description.trim();
+        }
+    }
+
+}
