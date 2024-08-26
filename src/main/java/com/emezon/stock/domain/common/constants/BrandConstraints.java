35e0dca828163adbfcb445d187438bc8a4bb0cbf
Jeison David Moncayo Marzola <desarrollo5ude@udea.edu.co>
2024-08-25 19:12:37 -0500

    Created BrandConstraints

---
+++ src/main/java/com/emezon/stock/domain/common/constants/BrandConstraints.java
@@ -0,0 +1,12 @@
+package com.emezon.stock.domain.common.constants;
+
+public class BrandConstraints {
+
+    public static final int NAME_MAX_LENGTH = 50;
+    public static final int DESCRIPTION_MAX_LENGTH = 120;
+
+    private BrandConstraints() {
+        throw new IllegalStateException("Utility class");
+    }
+
+}
