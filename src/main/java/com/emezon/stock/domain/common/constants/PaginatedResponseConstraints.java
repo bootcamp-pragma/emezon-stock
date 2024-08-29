d8edaf1b1372eb265b9ac4438411ef0b0a280ab1
Jeison David Moncayo Marzola <desarrollo5ude@udea.edu.co>
2024-08-29 00:01:29 -0500

    Updated PaginatedResponseConstraints

--- src/main/java/com/emezon/stock/domain/common/constants/PaginatedResponseConstraints.java
+++ src/main/java/com/emezon/stock/domain/common/constants/PaginatedResponseConstraints.java
@@ -6,7 +6,7 @@ public class PaginatedResponseConstraints {
 
     public static final int PAGE_SIZE_MIN = 1;
     public static final int PAGE_NUMBER_MIN = 0;
-    public static final List<String> SORT_DIRECTIONS = List.of("none", "asc", "desc");
+    public static final List<String> SORT_DIRECTIONS = List.of("asc", "desc");
 
     private PaginatedResponseConstraints() {
         throw new IllegalStateException("Utility class");
