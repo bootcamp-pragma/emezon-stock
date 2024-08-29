d7b2041aefae65131c63d8f8dafdf0490c0eab7d
Jeison David Moncayo Marzola <desarrollo5ude@udea.edu.co>
2024-08-29 00:22:06 -0500

    Deleten prefix CATGORY form constants

--- src/main/java/com/emezon/stock/domain/common/constants/PaginatedResponseErrorMessages.java
+++ src/main/java/com/emezon/stock/domain/common/constants/PaginatedResponseErrorMessages.java
@@ -2,9 +2,9 @@ package com.emezon.stock.domain.common.constants;
 
 public class PaginatedResponseErrorMessages {
 
-    public static final String CATEGORY_PAGE_SIZE_INVALID = "Page size must be greater than 0";
-    public static final String CATEGORY_PAGE_NUMBER_INVALID = "Page number must be greater than or equal to 0";
-    public static final String CATEGORY_SORT_DIRECTION_INVALID = "Sort direction must be 'none', 'asc' or 'desc'";
+    public static final String PAGE_SIZE_INVALID = "Page size must be greater than 0";
+    public static final String PAGE_NUMBER_INVALID = "Page number must be greater than or equal to 0";
+    public static final String SORT_DIRECTION_INVALID = "Sort direction must be 'none', 'asc' or 'desc'";
 
     private PaginatedResponseErrorMessages() {
         throw new IllegalStateException("Utility class");
