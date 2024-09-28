15ada571e1082f55bd20fa5e8770e6a38b2d87f7
Jeison David Moncayo Marzola <desarrollo5ude@udea.edu.co>
2024-09-28 10:07:09 -0500

    Updated Exceptionresponse

--- src/main/java/com/emezon/stock/app/errorhandling/ExceptionResponse.java
+++ src/main/java/com/emezon/stock/app/errorhandling/ExceptionResponse.java
@@ -8,17 +8,15 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 public class ExceptionResponse {
-    private String errorMessage;
+    private String message;
     private String requestedURI;
-    private Integer errorCode;
-    private String details;
+    private Integer code;
     private LocalDateTime timestamp;
 
-    public ExceptionResponse(String errorMessage, String requestedURI, Integer errorCode, String details) {
-        this.errorMessage = errorMessage;
+    public ExceptionResponse(String message, String requestedURI, Integer code) {
+        this.message = message;
         this.requestedURI = requestedURI;
-        this.errorCode = errorCode;
-        this.details = details;
+        this.code = code;
         this.timestamp = LocalDateTime.now();
     }
 }
