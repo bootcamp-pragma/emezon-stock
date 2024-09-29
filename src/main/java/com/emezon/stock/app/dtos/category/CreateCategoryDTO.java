ea51d88bf40e448f0e9f2e8d5646a58b55faca6c
Jeison David Moncayo Marzola <desarrollo5ude@udea.edu.co>
2024-09-29 07:52:14 -0500

    Added category validations to CreateCategoryDTO

--- src/main/java/com/emezon/stock/app/dtos/category/CreateCategoryDTO.java
+++ src/main/java/com/emezon/stock/app/dtos/category/CreateCategoryDTO.java
@@ -1,8 +1,11 @@
 package com.emezon.stock.app.dtos.category;
+import com.emezon.stock.domain.constants.CategoryConstraints;
 import com.emezon.stock.domain.constants.CategoryErrorMessages;
 import jakarta.annotation.PostConstruct;
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
 import lombok.*;
+import org.hibernate.validator.constraints.Length;
 
 @Getter
 @Setter
@@ -10,9 +13,11 @@ import lombok.*;
 public class CreateCategoryDTO {
 
     @NotBlank(message = CategoryErrorMessages.CATEGORY_NAME_REQUIRED)
+    @Length(max = CategoryConstraints.NAME_MAX_LENGTH, message = CategoryErrorMessages.CATEGORY_NAME_TOO_LONG)
     private String name;
 
     @NotBlank(message = CategoryErrorMessages.CATEGORY_DESCRIPTION_REQUIRED)
+    @Length(max = CategoryConstraints.DESCRIPTION_MAX_LENGTH, message = CategoryErrorMessages.CATEGORY_DESCRIPTION_TOO_LONG)
     private String description;
 
     @NotBlank(message = CategoryErrorMessages.CATEGORY_CODE_REQUIRED)
--- src/main/java/com/emezon/stock/domain/exceptions/category/CategoryDescriptionMaxLengthException.java
+++ src/main/java/com/emezon/stock/domain/exceptions/category/CategoryDescriptionMaxLengthException.java
@@ -4,6 +4,6 @@ import com.emezon.stock.domain.constants.CategoryErrorMessages;
 
 public class CategoryDescriptionMaxLengthException extends RuntimeException {
     public CategoryDescriptionMaxLengthException(Integer maxLength) {
-        super(String.format(CategoryErrorMessages.CATEGORY_DESCRIPTION_MAX_LENGTH, maxLength));
+        super(String.format(CategoryErrorMessages.CATEGORY_DESCRIPTION_TOO_LONG, maxLength));
     }
 }
