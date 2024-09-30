f342686b40d420a7e71fec21d030cb1a2f467dae
Jeison David Moncayo Marzola <desarrollo5ude@udea.edu.co>
2024-09-29 19:02:31 -0500

    Added brand controller validations

--- src/main/java/com/emezon/stock/domain/constants/BrandErrorMessages.java
+++ src/main/java/com/emezon/stock/domain/constants/BrandErrorMessages.java
@@ -5,9 +5,9 @@ public class BrandErrorMessages {
     public static final String BRAND_NAME_ALREADY_EXISTS = "Brand with name '%s' already exists";
     public static final String BRAND_NAME_ALREADY_EXISTS_DETAILS = "Brand name must be unique. Please try again with a different name.";
     public static final String BRAND_NAME_REQUIRED = "Brand name is required";
-    public static final String BRAND_NAME_MAX_LENGTH = "Brand name must not exceed %d characters";
+    public static final String BRAND_NAME_TOO_LONG = "Brand name must not exceed %d characters";
     public static final String BRAND_DESCRIPTION_REQUIRED = "Brand description is required";
-    public static final String BRAND_DESCRIPTION_MAX_LENGTH = "Brand description must not exceed %d characters";
+    public static final String BRAND_DESCRIPTION_TOO_LONG = "Brand description must not exceed %d characters";
     public static final String BRAND_NOT_FOUND_BY_ID = "Brand with id '%s' not found";
 
     private BrandErrorMessages() {
--- src/main/java/com/emezon/stock/domain/exceptions/brand/BrandNameMaxLengthException.java
+++ src/main/java/com/emezon/stock/domain/exceptions/brand/BrandNameMaxLengthException.java
@@ -4,6 +4,6 @@ import com.emezon.stock.domain.constants.BrandErrorMessages;
 
 public class BrandNameMaxLengthException extends RuntimeException {
     public BrandNameMaxLengthException(Integer maxLength) {
-        super(String.format(BrandErrorMessages.BRAND_NAME_MAX_LENGTH, maxLength));
+        super(String.format(BrandErrorMessages.BRAND_NAME_TOO_LONG, maxLength));
     }
 }
